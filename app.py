b67e14fc61e9c96b6796ad60228fb9cae1f37b38
Yukirona <81645346+Yukirona@users.noreply.github.com>
2023-02-27 16:04:11 +0100

    prof version

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
 from os import environ
-app = Flask(__name__)
+from flask import Flask, jsonify
+import logging
 
-@app.route('/')
-def hello_world():
-    msg = {"message":environ.get('MESSAGE')}
-    return msg
+app = Flask('motd')
 
-app.run('0.0.0.0',port=environ.get('APP_PORT'))
+app.logger.setLevel(logging.DEBUG)
+
+MESSAGE = environ.get("MESSAGE", "this is a fallback message")
+PORT = int(environ.get("PORT", "8080"))
+
+app.logger.info("message is [{}]".format(MESSAGE))
+app.logger.info("listen port is [{}]".format(PORT))
+
+@app.get('/')
+def get_motd():
+    return jsonify({"message": MESSAGE})
+
+if __name__ == '__main__':
+    
+    app.run(debug=True, port=PORT, host="0.0.0.0")
